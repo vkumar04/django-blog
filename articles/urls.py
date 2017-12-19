f71b50894109974f710a9a11de403fd6fa85b751
Vick Kumar <vick.kumar44@gmail.com>
2017-12-19 14:20:48 -0500

    created article app

---
+++ articles/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.article_list, name='list'),
+    path('<slug:slug>/', views.article_detail, name='detail')
+]
